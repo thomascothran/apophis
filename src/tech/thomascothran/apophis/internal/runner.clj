8c604bcec6d085b6c3028bd97fa224e1bac81136
Thomas Cothran <tmcothran@gmail.com>
2025-04-12 20:11:17 -0400

    Return more information from test runs

--- src/tech/thomascothran/apophis/internal/runner.clj
+++ src/tech/thomascothran/apophis/internal/runner.clj
@@ -5,10 +5,15 @@
   [test-fn testable-fn mutations]
   (let [original-form (get testable-fn :form)
         form-ns       (get testable-fn :namespace)
+        fn-symbol     (get testable-fn :symbol)
         mutations     (p/spawn-mutants mutations original-form)]
     (mapv (fn [mutation]
             (binding [*ns* form-ns] (eval mutation)
-                     (try (assoc (test-fn) :mutation mutation)
+                     (try (assoc (test-fn)
+                                 :mutation mutation
+                                 :ns (symbol (str form-ns))
+                                 :symbol fn-symbol
+                                 :original-form original-form)
                           (catch Exception e
                             {:exception e
                              :mutation mutation}))))
